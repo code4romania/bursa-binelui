c6e59f10b31bcb3e4aecf2349200182c12f49be3
cristosul <121957997+cristosul@users.noreply.github.com>
2023-01-25 22:07:10 +0200

    Login and logout

---
+++ src/Controller/Website/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Website;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'frontend_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'frontend_logout', methods: ['GET'])]
+    public function logout()
+    {
+
+    }
+}
