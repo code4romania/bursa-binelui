82150f2d4a35df1bcf6969734fd40bb6c379169d
FlorinZarafu <zrflorin@gmail.com>
2023-05-08 13:23:52 +0300

    Create nav link component

---
+++ resources/js/Components/links/NavLink.vue
@@ -0,0 +1,32 @@
+<template>
+    <Link
+        :href="href"
+        :class="[`${classes} inline-flex items-center text-base font-medium leading-5`]"
+    >
+        <slot />
+    </Link>
+</template>
+
+<script setup>
+    /** Import from vue. */
+    import { computed } from 'vue';
+
+    /** Import from inertia. */
+    import { Link } from '@inertiajs/vue3';
+
+    /** Component props. */
+    const props = defineProps({
+        href: {
+            type: String,
+            required: true,
+        },
+        active: {
+            type: Boolean,
+        },
+    });
+
+    /** Active link classes. */
+    const classes = computed(() =>
+        props.active ? 'text-turqoise-500 hover:text-turqoise-400' : 'text-gray-500 hover:text-turqoise-500'
+    );
+</script>
