1f5fb84227e64f3814f7e89a091f4965c612c7ea
FlorinZarafu <zrflorin@gmail.com>
2023-05-08 19:05:06 +0300

    Added projects route

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/projects', function () {
+    return Inertia::render('Projects');
+})->middleware(['auth', 'verified'])->name('projects');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
