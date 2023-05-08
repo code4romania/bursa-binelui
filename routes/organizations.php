e33d36cc9bc4be3b82881ed468aa98ea16c34db7
Cosmin Bosutar <cosmin.bosutar@genuineq.com>
2023-05-08 16:22:54 +0300

    View organization route update.

--- routes/organizations.php
+++ routes/organizations.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('organizatii', [OrganizationController::class, 'index'])->name('organizations');
 
-    Route::get('organizatie', [OrganizationController::class, 'show'])->name('organization');
+    Route::get('organizatie/{organization}', [OrganizationController::class, 'show'])->name('organization');
 });
 
 Route::middleware('auth')->group(function () {
