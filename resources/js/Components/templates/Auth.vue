936baad7287646d4b0ad4fdfcf7f860558f5db37
FlorinZarafu <zrflorin@gmail.com>
2023-05-08 16:09:09 +0300

    Added template for auth pages

---
+++ resources/js/Components/templates/Auth.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex min-h-full flex-1">
+        <div class="flex flex-1 flex-col px-4 mt-20 sm:px-6 lg:flex-none lg:px-20 xl:px-24 mx-20">
+            <div class="mx-auto w-full max-w-sm lg:w-96 mb-64">
+
+                <div>
+                    <h1 class="mb-6 text-6xl font-extrabold leading-9 tracking-tight text-cyan-900">{{ content.title }}</h1>
+
+                    <p class="text-sm leading-6 text-gray-600">
+                        {{ content.description }}
+                        <a :href="content.link.href" class="text-turqoise-500 hover:text-turqoise-400">{{ content.link.text }}</a>
+                    </p>
+
+                </div>
+                <slot />
+            </div>
+        </div>
+
+        <div class="relative hidden w-0 flex-1 lg:block">
+            <img class="absolute inset-0 h-full w-full object-cover" src="/images/auth_image.jpg" alt="Auth image" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    /** Component props. */
+    defineProps({
+        content: { type: Object }
+    });
+</script>
