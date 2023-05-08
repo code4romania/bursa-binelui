4fd7246f2c29588286e6f46f509b64fe45f84475
FlorinZarafu <zrflorin@gmail.com>
2023-05-08 17:32:05 +0300

    Create user dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,22 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
-    <Head title="Dashboard" />
+    <PageLayout>
+         <!-- Inertia page head -->
+        <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
-        </div>
-    </AuthenticatedLayout>
+        <!-- Dashboard template -->
+        <Dashboard>
+           <div class="p-9">
+                Dashboard
+           </div>
+        </Dashboard>
+    </PageLayout>
 </template>
+
+<script setup>
+    /** Import from inertia. */
+    import { Head } from '@inertiajs/vue3';
+
+    /** Import components. */
+    import PageLayout from '@/Layouts/PageLayout.vue';
+    import Dashboard from '@/Components/templates/Dashboard.vue';
+</script>
