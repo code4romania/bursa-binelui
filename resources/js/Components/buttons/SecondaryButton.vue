c07e26b5b2570cfb74da10a4956d6beffa19f969
FlorinZarafu <zrflorin@gmail.com>
2023-05-11 13:26:43 +0300

    Remove y padding from button

--- resources/js/Components/buttons/SecondaryButton.vue
+++ resources/js/Components/buttons/SecondaryButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         :type="type"
-        class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        class="rounded-md bg-white px-3.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
     >
         <slot />
     </button>
