e6d332a848d4dfdf512cb2599a542e7735e7b1d4
FlorinZarafu <zrflorin@gmail.com>
2023-05-16 18:43:26 +0300

    Backend organization edit

---
+++ app/Http/Requests/Organization/UpdateOrganizationRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Organization;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOrganizationRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
