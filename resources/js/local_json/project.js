2b6d8163c5e65ed696f7c0221975bf792aab4104
FlorinZarafu <zrflorin@gmail.com>
2023-05-18 14:26:47 +0300

    Added temporary json for project data

---
+++ resources/js/local_json/project.js
@@ -0,0 +1,55 @@
+export default {
+    "id": 3,
+    "name": "Zâmbete sănătoase pentru toți copiii",
+    "current_amount": 152200,
+    "max_amount": 202200,
+    "status":"published",
+    "active": true,
+    "logo": null,
+    "donation": 25,
+    "period_start": "2023-05-17",
+    "period_end": "2023-05-20",
+    "description": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+    "scope": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+    "beneficiary": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+    "why_donate": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+    "county_id": 20,
+    "city_id": 88653,
+    "city_name": "Bretea Română",
+    "county_name": "Hunedoara",
+    "city": {
+        "id": 88653,
+        "county_id": 20,
+        "level": 3,
+        "type": 22,
+        "parent_id": 88644,
+        "name": "Bretea Română"
+    },
+    "county": {
+        "id": 20,
+        "siruta": 207,
+        "name": "Hunedoara"
+    },
+    "activity_domains": [
+        "Securitate alimentară",
+        "Sănătate",
+        "Advocacy politici publice"
+    ],
+    "ong": {
+        "id": 1,
+        "name": "Zâmbete sănătoase pentru toți copiii",
+        "description": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "logo": "/images/ong.png"
+    },
+    "gallery":[
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    ]
+}
---
+++ resources/js/local_json/projects.js
@@ -0,0 +1,134 @@
+export default [
+    {
+        "id": 1,
+        "ong": {
+            "name":'Asociația Un zâmbet pentru copilul tău 1'
+        },
+        "name": "Zâmbete sănătoase pentru toți copiii 1",
+        "scor": 3432,
+        "troffes": 4,
+        "current_amount": 202200,
+        "max_amount": 202200,
+        "status":"published",
+        "active": true,
+        "logo": null,
+        "image_url": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        "donation": 25,
+        "period_start": "2023-05-21",
+        "period_end": "2023-05-22",
+        "description": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "scope": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "beneficiary": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "why_donate": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "county_id": 20,
+        "city_id": 88653,
+        "city_name": "Bretea Română",
+        "county_name": "Hunedoara",
+        "city": {
+            "id": 88653,
+            "county_id": 20,
+            "level": 3,
+            "type": 22,
+            "parent_id": 88644,
+            "name": "Bretea Română"
+        },
+        "county": {
+            "id": 20,
+            "siruta": 207,
+            "name": "Hunedoara"
+        },
+        "activity_domains": [
+            "Securitate alimentară",
+            "Sănătate",
+            "Advocacy politici publice"
+        ],
+    },
+    {
+        "id": 3,
+        "ong": {
+            "name":'Asociația Un zâmbet pentru copilul tău'
+        },
+        "name": "Zâmbete sănătoase pentru toți copiii",
+        "scor": 3432,
+        "troffes": 4,
+        "current_amount": 152200,
+        "max_amount": 202200,
+        "status":"draft",
+        "active": true,
+        "logo": null,
+        "image_url": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        "donation": 25,
+        "period_start": "2023-05-13",
+        "period_end": "2023-05-16",
+        "description": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "scope": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "beneficiary": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "why_donate": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "county_id": 20,
+        "city_id": 88653,
+        "city_name": "Bretea Română",
+        "county_name": "Hunedoara",
+        "city": {
+            "id": 88653,
+            "county_id": 20,
+            "level": 3,
+            "type": 22,
+            "parent_id": 88644,
+            "name": "Bretea Română"
+        },
+        "county": {
+            "id": 20,
+            "siruta": 207,
+            "name": "Hunedoara"
+        },
+        "activity_domains": [
+            "Securitate alimentară",
+            "Sănătate",
+            "Advocacy politici publice"
+        ],
+    },
+    {
+        "id": 4,
+        "ong": {
+            "name":'Asociația Un zâmbet pentru copilul tău'
+        },
+        "name": "Zâmbete sănătoase pentru toți copiii",
+        "scor": 3432,
+        "troffes": 4,
+        "current_amount": 202200,
+        "max_amount": 202200,
+        "status":"published",
+        "active": false,
+        "logo": null,
+        "image_url": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        "donation": 25,
+        "period_start": "2023-05-21",
+        "period_end": "2023-05-22",
+        "description": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "scope": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "beneficiary": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "why_donate": "Quia enim aut possimus dignissimos consequatur iusto. Ea omnis laborum nihil voluptas fugiat. Corrupti molestiae tempore et placeat eius voluptatum nihil qui. Sed nobis quaerat et hic voluptates. Nemo quisquam qui facere odio ad aut vero. Dolorum esse voluptatibus in consequatur nostrum qui. Cum et rerum laborum perferendis nulla vitae. Rerum qui sint odio. Molestiae consequatur explicabo rerum quis. Sunt dolore ad neque corrupti rerum. Unde dolorum dolorem autem.",
+        "county_id": 20,
+        "city_id": 88653,
+        "city_name": "Bretea Română",
+        "county_name": "Hunedoara",
+        "city": {
+            "id": 88653,
+            "county_id": 20,
+            "level": 3,
+            "type": 22,
+            "parent_id": 88644,
+            "name": "Bretea Română"
+        },
+        "county": {
+            "id": 20,
+            "siruta": 207,
+            "name": "Hunedoara"
+        },
+        "activity_domains": [
+            "Securitate alimentară",
+            "Sănătate",
+            "Advocacy politici publice"
+        ],
+    }
+]
