231d71f8a9720918bbd5ddd727f89b5c51856f75
FlorinZarafu <zrflorin@gmail.com>
2023-05-18 14:28:28 +0300

    Added layout pages

--- resources/js/Pages/Public/Donor/Donor.vue
+++ resources/js/Pages/Public/Donor/Donor.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Doantor</div>
+    <PageLayout>
+        <!-- Inertia page head -->
+        <Head :title="$t('thank_you')" />
+
+        <div class="p-9 mx-auto max-w-7xl mb-10">
+
+        </div>
+    </PageLayout>
 </template>
+
+<script setup>
+    /** Remove this import after backend connection. */
+    import project from '@/local_json/project.js';
+
+    /** Import from inertia. */
+    import { Head, Link } from '@inertiajs/vue3';
+
+    /** Import components. */
+    import PageLayout from '@/Layouts/PageLayout.vue';
+</script>
--- resources/js/Pages/Public/Website/About.vue
+++ resources/js/Pages/Public/Website/About.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        About
-    </div>
+    <PageLayout>
+        <!-- Inertia page head -->
+        <Head :title="$t('thank_you')" />
+
+        <div class="p-9 mx-auto max-w-7xl mb-10">
+
+        </div>
+    </PageLayout>
 </template>
+
+<script setup>
+    /** Remove this import after backend connection. */
+    import project from '@/local_json/project.js';
+
+    /** Import from inertia. */
+    import { Head, Link } from '@inertiajs/vue3';
+
+    /** Import components. */
+    import PageLayout from '@/Layouts/PageLayout.vue';
+</script>
