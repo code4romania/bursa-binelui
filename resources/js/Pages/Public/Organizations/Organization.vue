2ef3612606fe14897331369f115363cdef347914
FlorinZarafu <zrflorin@gmail.com>
2023-05-18 14:30:05 +0300

    Fix organization inertia data get

--- resources/js/Pages/Public/Organizations/Organization.vue
+++ resources/js/Pages/Public/Organizations/Organization.vue
@@ -5,12 +5,15 @@
 
         <div class="bg-white mb-20">
 
+            <!-- Header -->
             <div class="flex flex-col-reverse lg:flex-row mx-auto w-full lg:max-w-7xl mb-8 px-9">
                 <div class="w-full lg:w-6/12 flex flex-col justify-center xl:pr-6">
 
                     <div class="border-b border-gray-200 bg-white mr-6 py-4">
                         <div class="flex items-center gap-4">
-                            <SvgLoader name="global" />
+                            <div class="bg-turqoise-500 w-9 h-9 rounded-lg flex items-center justify-center">
+                                <SvgLoader class="shrink-0 fill-turqoise-500 stroke-turqoise-500" name="global" />
+                            </div>
                             <h3 class="text-base font-semibold leading-6 text-gray-900">{{ organization.county_name }}, {{ organization.city_name }}</h3>
                         </div>
                     </div>
@@ -19,18 +22,18 @@
 
                     <div class="flex items-center gap-4">
                         <Link
-                            href="route('projects')"
-                            class="text-center text-white bg-turqoise-500 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                            :href="route('projects')"
+                            class="text-center inline text-white bg-turqoise-500 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm"
                         >
                             {{ $t('see_projects') }}
                         </Link>
 
-                        <Link
-                            href="route('donate')"
-                            class="rounded-md bg-white text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 px-3.5 py-2.5"
-                        >
-                            {{ $t('volunteer') }}
-                        </Link>
+                        <!-- Volunteer modal -->
+                        <VolunteerModal
+                            triggerModalClasses="rounded-md bg-white text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 px-3.5 py-2.5"
+                            :triggerModalText="$t('become_volunter')"
+                            :data="organization"
+                        />
                     </div>
                 </div>
 
@@ -46,27 +49,16 @@
                 </div>
             </div>
 
+            <!-- Soacial share -->
             <div class="flex flex-col lg:flex-row mx-auto max-w-7xl mb-8 px-9">
                 <div class="w-full lg:w-6/12">
 
                     <h2 class="text-cyan-900 text-3xl font-bold mb-8">{{ $t('share_page') }}</h2>
-                    <div class="flex items-center gap-6 mb-12">
-                        <a :href="`https://www.facebook.com/sharer/sharer.php?u=${route('organization', organization.id)}`" target="_blank" rel="noopener">
-                            <SvgLoader class="shrink-0" name="facebook_colored" />
-                        </a>
-
-                        <a :href="`https://www.linkedin.com/sharing/share-offsite/?url=${route('organization', organization.id)}`" target="_blank" rel="noopener">
-                            <SvgLoader class="shrink-0" name="linkedin_colored" />
-                        </a>
-
-                        <a :href="`https://api.whatsapp.com/send?text=${route('organization', organization.id)}`" data-action="share/whatsapp/share"   target="_blank" rel="noopener">
-                            <SvgLoader class="shrink-0" name="whatsapp_colored" />
-                        </a>
-
-
-                        <SvgLoader @click="copyEmbed" class="shrink-0" name="embed_colored" />
-
-                    </div>
+                    <SharePage
+                        class="mb-20"
+                        :id="organization.id"
+                        pageRoute="organization"
+                    />
 
                     <div v-if="organization.activity_domains">
                         <h2 class="text-cyan-900 text-3xl font-bold mb-8">{{ $t('activity_domains') }}</h2>
@@ -127,7 +119,9 @@
             <!-- Projects -->
             <div class="bg-gray-50 px-9">
                 <div class="mx-auto max-w-7xl flex items-center gap-4 py-12">
-                    <SvgLoader class="shrink-0" name="brand_icon" />
+                    <div class="bg-turqoise-500 w-9 h-9 rounded-lg flex items-center justify-center">
+                        <SvgLoader class="shrink-0 fill-turqoise-500 stroke-turqoise-500" name="brand_icon" />
+                    </div>
                     <h3 class="text-gray-900 font-bold text-2xl">{{ $t('ong_projects') }}</h3>
                 </div>
             </div>
@@ -144,61 +138,30 @@
                 </ul>
             </div>
 
-            <div class="bg-turqoise-500 pt-12 pb-20 mt-16 px-9 lg:px-0">
-                <div class="mx-auto max-w-7xl flex items-center gap-4">
-                    <SvgLoader class="shrink-0" name="sound" />
-                    <h3 class="text-white font-bold text-2xl">{{ $t('ong_projects') }}</h3>
-                </div>
-            </div>
+            <!-- How can you help -->
+            <HowCanYouHelp
+                class="mb-20"
+                :id="organization.id"
+                pageRoute="organization"
+                @donate="triggerDonate"
+                @volunteer="triggerVolunteer"
+                @copyCode="copyEmbed"
 
-            <div class="bg-white px-9">
-                <ul role="list" class="mx-auto max-w-7xl grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-                    <li class="col-span-1 -mt-12 p-6 flex flex-col rounded-md bg-white shadow-lg">
-                        <SvgLoader name="share" />
-                        <h3 class="mt-6 mb-2 text-lg font-medium text-gray-900">{{ $t('social_media_share') }}</h3>
-                        <div class="text-sm text-gray-500">
-                            {{  $t('share')  }}
-                            <a class="text-blue-500 underline" :href="`https://www.facebook.com/sharer/sharer.php?u=${route('organization', organization.id)}`" target="_blank" rel="noopener">{{ $t('facebook') }}</a>,
-                            <a class="text-blue-500 underline" :href="`https://www.instagram.com/create/story/?text=${route('organization', organization.id)}`" target="_blank" rel="noopener">{{ $t('instagram') }}</a>,
-                            <a class="text-blue-500 underline" :href="`https://www.facebook.com/sharer/sharer.php?u=${route('organization', organization.id)}`" target="_blank" rel="noopener">{{ $t('linkedin') }}</a>,
-                            {{ $t('send_to_friend') }}
-                            <a class="text-blue-500 underline" :href="`https://api.whatsapp.com/send?text=${route('organization', organization.id)}`" data-action="share/whatsapp/share"   target="_blank" rel="noopener">{{ $t('whatsapp') }}</a>
-                            {{ $t('or') }}
-                            <a class="text-blue-500 underline" :href="`https://www.facebook.com/dialog/send?app_id=APP_ID&link=${route('organization', organization.id)}`" target="_blank" rel="noopener">{{ $t('messenger') }}</a>
-                            {{ $t('tell_about') }}
-                        </div>
-                    </li>
-
-                    <li class="col-span-1 -mt-12 p-6 flex flex-col rounded-md bg-white shadow-lg">
-                        <SvgLoader name="heart" />
-                        <h3 class="mt-6 mb-2 text-lg font-medium text-gray-900">{{ $t('donate_or_volunteer') }}</h3>
-                        <div class="text-sm text-gray-500">
-                            {{ $t('interested_in_ong') }}
-                            <Link class="text-blue-500 underline" href="route('donate')">{{ $t('donate') }}</Link>
-                            {{ $t('help_them') }}
-                            <Link class="text-blue-500 underline" href="route('donate')">{{ $t('register_to_volunteer') }}</Link>
-                            {{ $t('for_ong') }}
-                        </div>
-                    </li>
-
-                    <li class="col-span-1 -mt-12 p-6 flex flex-col rounded-md bg-white shadow-lg">
-                        <SvgLoader name="glob" />
-                        <h3 class="mt-6 mb-2 text-lg font-medium text-gray-900">{{ $t('your_blog') }}</h3>
-                        <div class="text-sm text-gray-500">
-                            {{ $t('you_have_blog') }}
-                            <a class="text-blue-500 underline" href="">{{ $t('embed_code') }}</a>
-                            {{ $t('copy_code') }}
-                        </div>
-                    </li>
-                </ul>
-            </div>
+            />
         </div>
+
+        <!-- Donate modal -->
+        <DonateModal
+            triggerModalClasses="h-0"
+            triggerModalText=""
+            :data="organization"
+        />
     </PageLayout>
 </template>
 
 <script setup>
-    /** Import form vue */
-    import { computed } from 'vue';
+    /** Remove this import after backend connection. */
+    import projects from '@/local_json/projects.js';
 
     /** Import from inertia. */
     import { Head, Link, usePage } from '@inertiajs/vue3';
@@ -206,13 +169,16 @@
     /** Import components. */
     import PageLayout from '@/Layouts/PageLayout.vue';
     import SvgLoader from '@/Components/SvgLoader.vue';
-    import ProjectCard from '@/Components/cards/ProjectCard.vue'
+    import ProjectCard from '@/Components/cards/ProjectCard.vue';
+    import DonateModal from '@/Components/modals/DonateModal.vue';
+    import VolunteerModal from '@/Components/modals/VolunteerModal.vue';
+    import SharePage from '@/Components/SharePage.vue';
+    import HowCanYouHelp from '@/Components/HowCanYouHelp.vue';
 
     /** Page props. */
-    const props = computed(() => usePage().props);
-
-    /** Organizations. */
-    const organization = computed(() => props.value ? props.value.organization : {});
+    const props = defineProps({
+        organization: [Array, Object]
+    });
 
     /**
      * Copy embed code.
@@ -248,49 +214,9 @@
         }
     }
 
-    /** Grid list */
-    const projects = [
-        {
-            id: 1,
-            scor: 3432,
-            troffes: 4,
-            name: 'Asociația MediuACUM',
-            title: 'Ecologizarea canalului de la marginea Tulcei',
-            county: "Alba Iulia",
-            activity: "Mediu",
-            currentAmount: 12200,
-            maxAmount: 202200,
-            status:"published",
-            active: true,
-            imageUrl:'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-        },
-        {
-            id: 2,
-            scor: 3432,
-            troffes: 4,
-            name: 'Asociația MediuACUM',
-            title: 'Ecologizarea canalului de la marginea Tulcei',
-            county: "Alba Iulia",
-            activity: "Mediu",
-            currentAmount: 12200,
-            maxAmount: 202200,
-            status:"published",
-            active: true,
-            imageUrl:'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-        },
-        {
-            id: 3,
-            scor: 3432,
-            troffes: 4,
-            name: 'Asociația MediuACUM',
-            title: 'Ecologizarea canalului de la marginea Tulcei',
-            county: "Alba Iulia",
-            activity: "Mediu",
-            currentAmount: 12200,
-            maxAmount: 202200,
-            status:"published",
-            active: true,
-            imageUrl:'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-        },
-    ];
+    /** Trigger volunteer modal from card. */
+    const triggerVolunteer = (() => { document.getElementById('volunteer-active-modal').click(); });
+
+    /** Trigger donate modal from card. */
+    const triggerDonate = (() => { document.getElementById('donate-active-modal').click(); });
 </script>
