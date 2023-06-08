c6a3ac0787bc2868a774785315f88ac473d60113
FlorinZarafu <zrflorin@gmail.com>
2023-06-08 16:34:14 +0300

    Fix auth template

--- resources/js/Components/templates/Auth.vue
+++ resources/js/Components/templates/Auth.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="flex min-h-full flex-1">
-        <div class="flex flex-1 flex-col px-4 mt-20 sm:px-6 lg:flex-none lg:px-20 xl:px-24 lg:mx-20">
-            <div class="mx-auto w-full mb-64">
+    <div class="grid min-h-full grid-cols-12">
+        <div class="flex flex-col col-span-5 px-4 mt-20 sm:px-6 lg:flex-none lg:px-20 xl:px-24 lg:mx-20">
+            <div class="w-full mx-auto mb-64">
 
                 <div>
-                    <h1 class="mb-6 text-6xl font-extrabold leading-9 tracking-tight text-cyan-900">{{ content.title }}</h1>
+                    <h1 v-if="content.hasOwnProperty('title')" class="text-6xl font-extrabold leading-9 tracking-tight text-cyan-900">{{ content.title }}</h1>
 
-                    <p class="text-sm leading-6 text-gray-600">
-                        {{ content.description }}
-                        <a :href="content.link.href" class="text-turqoise-500 hover:text-turqoise-400">{{ content.link.text }}</a>
+                    <p class="mt-6 text-sm leading-6 text-gray-600">
+                        <span v-if="content.hasOwnProperty('description')">{{ content.description }}</span>
+                        <a v-if="content.hasOwnProperty('link')" :href="content.link.href" class="text-turqoise-500 hover:text-turqoise-400">{{ content.link.text }}</a>
                     </p>
 
                 </div>
@@ -16,8 +16,8 @@
             </div>
         </div>
 
-        <div class="relative hidden w-0 flex-1 lg:block">
-            <img class="absolute inset-0 h-full w-full object-cover" src="/images/auth_image.jpg" alt="Auth image" />
+        <div class="relative hidden col-span-7 lg:block">
+            <img class="absolute inset-0 object-cover w-full h-full" src="/images/auth_image.jpg" alt="Auth image" />
         </div>
     </div>
 </template>
