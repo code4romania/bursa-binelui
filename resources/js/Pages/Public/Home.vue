f9424297fde7e73f377a525237c5e9a137340e64
FlorinZarafu <zrflorin@gmail.com>
2023-06-09 17:37:14 +0300

    Started home page

---
+++ resources/js/Pages/Public/Home.vue
@@ -0,0 +1,206 @@
+<template>
+    <PageLayout>
+        <!-- Inertia page head -->
+        <Head title="Acasa" />
+
+        <header class="flex flex-col-reverse w-full mx-auto lg:gap-10 xl:gap-20 lg:flex-row lg:max-w-7xl px-9">
+            <div class="w-full lg:w-6/12 lg:py-12">
+                <h1 class="relative z-50 text-2xl font-extrabold text-gray-900 lg:text-6xl">{{ $t('home_title') }}</h1>
+
+                <div class="relative flex flex-col items-center pb-10 my-10 md:flex-row gap-x-6">
+                    <Link
+                        :href="route('projects')"
+                        class="bg-turqoise-500 text-center z-50 flex-1 w-full sm:w-auto hover:bg-turqoise-400 text-white focus-visible:outline-turqoise-500 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    >
+                        {{ $t('register_project') }}
+                    </Link>
+
+                    <Link
+                        :href="route('projects')"
+                        class="rounded-md flex-1 bg-white text-center px-3.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 py-2.5"
+                    >
+                        {{ $t('see_projects') }}
+                    </Link>
+
+                    <div class="absolute top-0 hidden -left-16 md:block">
+                        <SvgLoader class="shrink-0 fill-turqoise-300" name="small_dotted_square" />
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center gap-16 mx-auto md:flex-row">
+                    <div>
+                        <h3 class="text-6xl font-bold text-gray-900">900+</h3>
+                        <p class="text-xl text-gray-900">{{ $t('projects') }}</p>
+                    </div>
+                    <div>
+                        <h3 class="text-6xl font-bold text-gray-900">700+</h3>
+                        <p class="text-xl text-gray-900">{{ $t('organizations') }}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:w-6/12">
+                <SvgLoader name="home_bg" class="w-full" />
+            </div>
+        </header>
+
+        <div class="relative pb-10 mt-10 overflow-hidden px-9">
+            <div class="w-full py-10 mx-auto rounded shadow-md lg:max-w-7xl lg:pl-32 lg:pr-10">
+
+                <div class="lg:max-w-6xl">
+                    <div class="flex items-center w-full border border-gray-300 divide-x-2">
+                        <div class="flex-1">s</div>
+                        <div class="flex-1">b</div>
+                    </div>
+
+
+                    <div class="inline-flex flex-col">
+                        <h3 class="inline px-6 py-2 text-2xl font-bold text-white bg-red-500 w-fit">ONG-URILE DE PE BURSA BINELUI</h3>
+                        <h3 class="inline px-6 py-2 text-xl font-bold text-white w-fit bg-turqoise-500">Se pot programa la CiviTech 911. Afla cum aici...</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="absolute bottom-0 right-0 hidden md:block">
+                <SvgLoader class="shrink-0 fill-turqoise-300" name="dotted_square" />
+            </div>
+        </div>
+
+        <div class="flex items-center justify-between w-full gap-6 mx-auto mt-10 lg:max-w-7xl px-9">
+
+           <div class="flex items-center gap-6">
+                <h2 class="text-2xl font-bold text-red-500 lg:text-5xl">Donează pentru fapte bune</h2>
+                <Link
+                    :href="route('projects')"
+                    class="bg-red-500 text-center z-50 w-full sm:w-auto hover:bg-red-400 text-white focus-visible:outline-red-500 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                    {{ $t('register_project') }}
+                </Link>
+           </div>
+
+           <div class="flex items-center gap-6">
+                <div @click="projects.prev()" class="flex items-center justify-center w-10 h-10 p-2 bg-white rounded-lg shadow-md cursor-pointer">
+                    <SvgLoader class="shrink-0 fill-gray-700" name="chevron_left"/>
+                </div>
+
+                <div @click="projects.next()" class="flex items-center justify-center w-10 h-10 p-2 bg-white rounded-lg shadow-md cursor-pointer">
+                    <SvgLoader class="shrink-0 fill-gray-700" name="chevron_right"/>
+                </div>
+           </div>
+        </div>
+
+        <div class="relative w-full">
+            <carousel
+                class="relative z-50 w-full"
+                v-bind="carouselOptions.settings"
+                :breakpoints="carouselOptions.breakpoints"
+                ref="projects"
+            >
+                <slide
+                    v-for="(project, index) in query.data"
+                    :key="index"
+                    class="flex flex-col py-9"
+                >
+                    <ProjectCard
+                        :class="[0 === index % 2 ? '-mt-9' : 'mt-9']"
+                        :data="project"
+                    />
+                </slide>
+            </carousel>
+
+            <div class="relative w-full mx-auto lg:max-w-7xl">
+                <div class="absolute bottom-0 hidden md:block left-60">
+                    <SvgLoader class="shrink-0 fill-turqoise-300" name="dotted_square" />
+                </div>
+            </div>
+        </div>
+
+
+        <div class="w-full mx-auto mt-10 rounded shadow-md lg:max-w-7xl px-9"></div>
+
+            <div class="relative w-full bg-white">
+                <div class="flex items-center justify-between w-full gap-6 mx-auto mt-10 lg:max-w-7xl px-9">
+                    <div class="flex items-center gap-6">
+                        <h2 class="text-2xl font-bold text-cyan-900 lg:text-5xl">BCR pentru comunitate</h2>
+                        <Link
+                            :href="route('projects')"
+                            class="bg-turqoise-500 text-center z-50 w-full sm:w-auto hover:bg-turqoise-400 text-white focus-visible:outline-turqoise-500 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                        >
+                            {{ $t('register_project') }}
+                        </Link>
+                    </div>
+
+                    <div class="flex items-center gap-6">
+                        <div @click="projects.prev()" class="flex items-center justify-center w-10 h-10 p-2 bg-white rounded-lg shadow-md cursor-pointer">
+                            <SvgLoader class="shrink-0 fill-gray-700" name="chevron_left"/>
+                        </div>
+
+                        <div @click="projects.next()" class="flex items-center justify-center w-10 h-10 p-2 bg-white rounded-lg shadow-md cursor-pointer">
+                            <SvgLoader class="shrink-0 fill-gray-700" name="chevron_right"/>
+                        </div>
+                    </div>
+                </div>
+
+                <carousel
+                    class="relative z-50 w-full"
+                    v-bind="carouselOptions.settings"
+                    :breakpoints="carouselOptions.breakpoints"
+                    ref="projects"
+                >
+                    <slide
+                        v-for="(project, index) in query.data"
+                        :key="index"
+                        class="flex flex-col py-9"
+                    >
+                        <ProjectCard
+                            :class="[0 === index % 2 ? '-mt-9' : 'mt-9']"
+                            :data="project"
+                        />
+                    </slide>
+                </carousel>
+            </div>
+    </PageLayout>
+</template>
+
+<script setup>
+    /** Import from vue */
+    import { ref } from 'vue';
+
+    /** Import from inertia. */
+    import { Head, Link, useForm, router } from '@inertiajs/vue3';
+
+    /** Import plugins */
+    import 'vue3-carousel/dist/carousel.css';
+    import { Carousel, Slide } from 'vue3-carousel';
+
+    /** Import components. */
+    import PageLayout from '@/Layouts/PageLayout.vue';
+    import SvgLoader from '@/Components/SvgLoader.vue';
+    import ProjectCard from '@/Components/cards/ProjectCard.vue'
+
+    /** Component props. */
+    const props = defineProps({
+        query: Object
+    });
+
+    const projects = ref(null)
+
+    /** Carousel options. */
+    const carouselOptions = ref({
+        settings: {
+        itemsToShow: 1,
+        snapAlign: 'start',
+        },
+        breakpoints: {
+            700: {
+                itemsToShow: 3.5,
+                snapAlign: 'start',
+            },
+            1024: {
+                itemsToShow: 5.5,
+                snapAlign: 'start',
+                wrapAround: true
+            }
+        }
+    });
+</script>
