16d514562d62870fd9fa8acedd0d0a64620d1f27
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-06-23 11:49:39 +0300

    wip on championship

--- app/Http/Controllers/ChampionshipController.php
+++ app/Http/Controllers/ChampionshipController.php
@@ -13,86 +13,15 @@ class ChampionshipController extends Controller
 {
     public function index()
     {
-        $testimonials = [
-            [
-                'content' => '11111 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.',
-                'name' => 'Judith Black',
-                'job' => 'CEO',
-                'company' => 'Workcation',
-            ],
-            [
-                'content' => '222222222 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.',
-                'name' => 'Judith Black',
-                'job' => 'CEO',
-                'company' => 'Workcation',
-            ],
-        ];
-
-        $links = [
-            [
-                'href' => '#',
-                'label' => 'Titlu Articol',
-                'source' => 'sursa.ro',
-            ],
-            [
-                'href' => '#',
-                'label' => 'Titlu Articol',
-                'source' => 'sursa.ro',
-            ],
-            [
-                'href' => '#',
-                'label' => 'Titlu Articol',
-                'source' => 'sursa.ro',
-            ],
-        ];
-
-        $editions = [
-            [
-                'href' => '1',
-                'name' => 'Campionatul de bine 2020',
-            ],
-            [
-                'href' => '2',
-                'name' => 'Campionatul de bine 2019',
-            ],
-            [
-                'href' => '3',
-                'name' => 'Campionatul de bine 2018',
-            ],
-        ];
-
-        $articles = [
-            [
-                'id' => 1,
-                'img' => '/images/project_img.png',
-                'author' => 'Ion Popescu',
-                'name' => 'Importanța educației remediare în România în timpul pandemiei',
-                'team' => 'Echipa BCR',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-                'date' => '15.02.2022',
-            ],
-            [
-                'id' => 2,
-                'img' => '/images/project_img.png',
-                'author' => 'Ion Popescu',
-                'name' => 'Importanța educației remediare în România în timpul pandemiei',
-                'team' => 'Echipa BCR',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-                'date' => '15.02.2022',
-            ],
-            [
-                'id' => 3,
-                'img' => '/images/project_img.png',
-                'author' => 'Ion Popescu',
-                'name' => 'Importanța educației remediare în România în timpul pandemiei',
-                'team' => 'Echipa BCR',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-                'date' => '15.02.2022',
-            ],
-        ];
 
         $projects = Project::publish()->paginate(9)->withQueryString();
-        $counties = County::whereHas('projects')->get(['name', 'id']);
+        $counties = County::get(['name', 'id']);
+        $testimonials=[];
+        $links=[];
+        $editions=[];
+        $articles=[];
+
+
 
         return Inertia::render('Public/Championship/Championship', [
             'query' => $projects,
@@ -208,7 +137,7 @@ class ChampionshipController extends Controller
         ];
 
         $projects = Project::publish()->paginate(9)->withQueryString();
-        $counties = County::whereHas('projects')->get(['name', 'id']);
+        $counties = County::get(['name', 'id']);
 
         return Inertia::render('Public/Championship/Edition', [
             'query' => $projects,
