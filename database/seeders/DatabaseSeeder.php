78b87b0b995db9b7c00629847a78b6b1caa1f1f9
Andrei Ioniță <hi@andrei.io>
2023-08-16 15:30:48 +0100

    cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,71 +4,85 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Enums\ActivityDomain as ActivityDomainEnum;
 use App\Models\ActivityDomain;
-use App\Models\Article;
+use App\Models\ArticleCategory;
+use App\Models\Badge;
 use App\Models\Championship;
 use App\Models\Organization;
 use App\Models\Project;
 use App\Models\User;
-use App\Models\Volunteer;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    public const USER_DONOR_NUMBER = 45;
-
-    public const USER_NGO_ADMIN_NUMBER = 50;
-
-    public const USER_BB_MANAGER_NUMBER = 1;
-
-    public const USER_BB_ADMIN_NUMBER = 1;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        if ('production' !== config('app.env', 'production')) {
-            $activityDomains = ActivityDomainEnum::values();
-            $tmpActivityDomains = [];
-            User::factory(['email' => 'bb-admin@example.com'])->bbAdmin()->create();
-            Championship::factory()->count(3)->create();
-            foreach ($activityDomains as $domain) {
-                $tmpActivityDomains[] = ['name' => $domain, 'slug' => \Str::slug($domain)];
-            }
-            ActivityDomain::insert($tmpActivityDomains);
-            for ($i = 0; $i < self::USER_DONOR_NUMBER; $i++) {
-                User::factory()->donor()->create();
-            }
+        if (app()->isProduction()) {
+            return;
+        }
 
-            for ($i = 0; $i < self::USER_NGO_ADMIN_NUMBER; $i++) {
-                $organization = Organization::factory(['eu_platesc_merchant_id'=>config('services.eu_platesc.merchant_id'), 'eu_platesc_private_key'=>config('services.eu_platesc.private_key')])->create();
-                if ($i === 0) {
-                    User::factory(['email' => 'admin@example.com'])->ngoAdmin()->for($organization)->create();
-                } else {
-                    User::factory()->ngoAdmin()->for($organization)->create();
-                }
-                $project = Project::factory()->for($organization)->count(10)->create();
-                $project->each(function ($project) {
-                    $project->volunteers()->attach(Volunteer::factory()->count(10)->create());
-                });
-            }
+        User::factory(['email' => 'bb-admin@example.com'])
+            ->bbAdmin()
+            ->create();
 
-            for ($i = 0; $i < self::USER_BB_MANAGER_NUMBER; $i++) {
-                User::factory()->bbManager()->create();
-            }
+        User::factory(['email' => 'bb-manager@example.com'])
+            ->count(1)
+            ->bbManager()
+            ->create();
 
-            for ($i = 0; $i < self::USER_BB_ADMIN_NUMBER; $i++) {
-                User::factory()->bbAdmin()->create();
-            }
+        User::factory()
+            ->count(50)
+            ->donor()
+            ->create();
 
-            $articleCategories = ['Social', 'Educație', 'Sănătate', 'Cultură', 'Mediu', 'Sport', 'Animale', 'Altele'];
-            foreach ($articleCategories as $category) {
-                \App\Models\ArticleCategory::factory(['name' => $category, 'slug' => \Str::slug($category)])
-                    ->has(Article::factory()->count(4))->create();
-            }
+        collect(ActivityDomainEnum::values())
+            ->map(fn ($domain) => [
+                'name' => $domain,
+                'slug' => Str::slug($domain),
+            ])
+            ->tap(function (Collection $collection) {
+                ActivityDomain::insert($collection->toArray());
+            });
+
+        Championship::factory()
+            ->count(3)
+            ->create();
+
+        Badge::factory()
+            ->count(30)
+            ->create();
+
+        Organization::factory()
+            ->count(50)
+            ->has(
+                User::factory()
+                    ->ngoAdmin()
+                    ->sequence(fn (Sequence $sequence) => [
+                        'email' => $sequence->index
+                            ? "admin-{$sequence->index}@example.com"
+                            : 'admin@example.com',
+                    ])
+            )
+            ->has(
+                Project::factory()
+                    ->count(10)
+                    ->hasVolunteers(10)
+            )
+            ->create();
+
+        $articleCategories = ['Social', 'Educație', 'Sănătate', 'Cultură', 'Mediu', 'Sport', 'Animale', 'Altele'];
+        foreach ($articleCategories as $category) {
+            ArticleCategory::factory()
+                ->name($category)
+                ->hasArticles(4)
+                ->create();
         }
     }
 }
