e26ca223d9700f30908b488bfa9648f98bbac67f
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-08-29 14:34:06 +0300

    fix welcome route

--- routes/auth.php
+++ routes/auth.php
@@ -30,6 +30,12 @@ Route::middleware('guest')->group(function () {
 
 Route::patch('usrupdate/{id}', [RegisteredUserController::class, 'update'])->name('user.update');
 Route::get('admin/welcome/{user}', Welcome::class)->name('filament.auth.welcome');
+Route::middleware(config('filament.middleware.base'))
+    ->domain(config('filament.domain'))
+    ->prefix(config('filament.path'))
+    ->group(function () {
+        Route::get('/welcome/{user}', Welcome::class)->name('filament.auth.welcome');
+    });
 Route::get('ngo/welcome/{user}', [PasswordController::class,'setInitialPassword'])->name('ngo.user.welcome');
 Route::post('ngo/welcome/{user}', [PasswordController::class,'storeInitialPassword'])->name('ngo.user.welcome.store');
 
