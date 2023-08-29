54dda915f2409a49ad7b1523d7c843b7dee4194f
Andrei Ioniță <hi@andrei.io>
2023-08-29 14:22:38 +0100

    cleanup

--- app/Filament/Resources/TicketResource/Pages/ViewTicket.php
+++ app/Filament/Resources/TicketResource/Pages/ViewTicket.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\TicketResource\Pages;
 
-use App\Filament\Resources\InterventionResource\Actions\ToggleStatusAction;
 use App\Filament\Resources\TicketResource;
+use App\Filament\Resources\TicketResource\Actions\ToggleStatusAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewTicket extends ViewRecord
