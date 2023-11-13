77d94f16aae3ba96a85eecb8f2e7b414554ef8cb
Andrei Ioniță <hi@andrei.io>
2023-11-13 15:34:04 +0000

    fix

--- config/filament-breezy.php
+++ config/filament-breezy.php
@@ -82,7 +82,7 @@ return [
     |--------------------------------------------------------------------------
     | Path to registration Livewire component.
     */
-    'registration_component_path' => Register::class,
+    'registration_component_path' => \JeffGreco13\FilamentBreezy\Http\Livewire\Auth\Register::class,
     /*
     |--------------------------------------------------------------------------
     | Path to password reset Livewire component.
