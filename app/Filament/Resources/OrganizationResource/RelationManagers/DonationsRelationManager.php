04cf11a0d522b660780745e3b4f8c1dcc247c12a
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-12-08 17:20:48 +0200

    fix donation for organization

--- app/Filament/Resources/OrganizationResource/RelationManagers/DonationsRelationManager.php
+++ app/Filament/Resources/OrganizationResource/RelationManagers/DonationsRelationManager.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\OrganizationResource\RelationManagers;
 
-use Filament\Forms;
+use App\Enums\EuPlatescStatus;
+use App\Models\Donation;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
-use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Illuminate\Database\Eloquent\Builder;
 
 class DonationsRelationManager extends RelationManager
 {
@@ -25,9 +30,16 @@ class DonationsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('uuid')
+                TextInput::make('uuid')
                     ->required()
                     ->maxLength(255),
+                TextInput::make('email')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('amount')
+                    ->required()
+                    ->maxLength(255),
+
             ]);
     }
 
@@ -35,20 +47,49 @@ class DonationsRelationManager extends RelationManager
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('uuid'),
+                TextColumn::make('id')
+                    ->label(__('donation.labels.id'))
+                    ->formatStateUsing(fn (Donation $record) => sprintf('#%d', $record->id)),
+                TextColumn::make('status')
+                    ->label(__('donation.labels.status'))
+                    ->formatStateUsing(fn (Donation $record) => $record->status->label()),
+                TextColumn::make('project.name')
+                    ->label(__('donation.labels.project')),
+                TextColumn::make('email')
+                    ->label(__('donation.labels.email')),
+                TextColumn::make('amount')
+                    ->label(__('donation.labels.amount')),
+                TextColumn::make('created_at')
+                    ->label(__('donation.labels.created_at'))->sortable(),
+
             ])
             ->filters([
-                //
+                SelectFilter::make('status')
+                    ->label(__('donation.labels.status'))
+                    ->options(EuPlatescStatus::options())
+                    ->multiple(),
+                SelectFilter::make('project_id')
+                    ->label(__('donation.labels.project'))
+                    ->relationship('project', 'name')
+                    ->multiple(),
+                TernaryFilter::make('has_user')
+                    ->label(__('donation.labels.has_user'))
+                    ->queries(
+                        true: fn (Builder $query) => $query->whereNotNull('user_id'),
+                        false: fn (Builder $query) => $query->whereNull('user_id'),
+                    ),
+                TernaryFilter::make('in_championship')
+                    ->label(__('donation.labels.in_championship'))
+                    ->queries(
+                        true: fn (Builder $query) => $query->whereHas('championshipStage'),
+                        false: fn (Builder $query) => $query->whereDoesntHave('championshipStage'),
+                    ),
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 }
