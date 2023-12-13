21ec76f4111395f9171285d49879aa9fa3c5e157
Andrei Ioniță <hi@andrei.io>
2023-12-13 16:08:08 +0000

    feat: prelaunch page (#305)
    * feat: prelaunch page
    
    * feat: configurable prelaunch secret

--- docker/s6-rc.d/laravel/init.sh
+++ docker/s6-rc.d/laravel/init.sh
@@ -3,10 +3,19 @@
 cd /var/www
 
 php artisan migrate --force
+php artisan storage:link
+
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
-php artisan storage:link
+php artisan icons:cache
+
+if [ -n "${PRELAUNCH_SECRET}" ]; then
+    php artisan down \
+        --secret="$PRELAUNCH_SECRET" \
+        --render="prelaunch" \
+        --status="200"
+fi
 
 php artisan about
