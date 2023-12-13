712be672f58e5a3f4145de436cd010e89a64bfa0
Andrei Ioniță <hi@andrei.io>
2023-12-13 15:25:18 +0000

    fix: add cloudfront proto header handling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\SetCloudfrontHeaders::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
---
+++ app/Http/Middleware/SetCloudfrontHeaders.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetCloudfrontHeaders
+{
+    protected string $header = 'Cloudfront-Forwarded-Proto';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->hasHeader($this->header)) {
+            $request->headers->add([
+                'X-Forwarded-Proto' => $request->header($this->header),
+            ]);
+        }
+
+        return $next($request);
+    }
+}
