77ea71ba1f7cd22a31fc41e5ddac467bd943dd12
Miruna Muscan <111346300+mirunaioana-msc@users.noreply.github.com>
2024-02-08 14:44:54 +0200

    Update custom_validation.php
    Update texts BB

--- lang/ro/custom_validation.php
+++ lang/ro/custom_validation.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'activity_domains_ids' => 'Trebuie să selectați cel puțin un domeniu de activitate.',
-    'counties_ids' => 'Trebuie să selectați cel puțin un județ.',
+    'counties_ids' => 'Te rugăm să selectezi cel puțin un județ.',
     'start_date' => [
-        'after_or_equal' => 'Data de început nu poate fi in trecut.',
+        'after_or_equal' => 'Data de început a perioadei de donații nu poate fi în trecut.',
     ],
 ];
