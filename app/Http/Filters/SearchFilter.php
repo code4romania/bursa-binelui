011d79b64dca9a5ba9ed571c34908439fc8d7b93
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-03-21 09:40:06 +0200

    fix search with comma

--- app/Http/Filters/SearchFilter.php
+++ app/Http/Filters/SearchFilter.php
@@ -6,16 +6,14 @@ namespace App\Http\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
-use function JmesPath\search;
 
 class SearchFilter implements Filter
 {
     public function __invoke(Builder $query, $term, string $property): void
     {
-       if(is_array($term))
-       {
-           $term = implode(',', $term);
-       }
+        if (\is_array($term)) {
+            $term = implode(',', $term);
+        }
         $query->search($term);
     }
 }
