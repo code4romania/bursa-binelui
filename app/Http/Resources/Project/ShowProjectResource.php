43c8e368e6f49adba4e29bdf1990eb74b531af41
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-05-02 11:54:02 +0300

    fix publish button

--- app/Http/Resources/Project/ShowProjectResource.php
+++ app/Http/Resources/Project/ShowProjectResource.php
@@ -55,6 +55,7 @@ class ShowProjectResource extends Resource
             })->toArray(),
             'is_active' => $this->is_active,
             'is_starting_soon' => $this->isStartingSoon(),
+            'active' => $this->active,
             'external_links' => collect($this->external_links)->map(function (array $link) {
                 $link['source'] = parse_url($link['url'], \PHP_URL_HOST);
 
