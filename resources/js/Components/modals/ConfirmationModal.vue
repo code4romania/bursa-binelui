695b1cb14ca0a36adb3d0232b8afb251493fc2fc
Andrei Ioniță <hi@andrei.io>
2024-05-17 15:14:19 +0300

    fix: wrap teleports in client-only (#367)
    * fix: wrap teleports in client-only
    
    * cleanup
    
    * cleanup

--- resources/js/Components/modals/ConfirmationModal.vue
+++ resources/js/Components/modals/ConfirmationModal.vue
@@ -8,81 +8,85 @@
             v-text="trigger"
         />
 
-        <Teleport to="body">
-            <TransitionRoot as="template" :show="isOpen">
-                <Dialog as="div" class="relative z-10" @close="close">
-                    <TransitionChild
-                        as="template"
-                        enter="ease-out duration-300"
-                        enter-from="opacity-0"
-                        enter-to="opacity-100"
-                        leave="ease-in duration-200"
-                        leave-from="opacity-100"
-                        leave-to="opacity-0"
-                    >
-                        <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
-                    </TransitionChild>
+        <ClientOnly>
+            <Teleport to="#notification-teleport-target">
+                <TransitionRoot as="template" :show="isOpen">
+                    <Dialog as="div" class="relative z-10" @close="close">
+                        <TransitionChild
+                            as="template"
+                            enter="ease-out duration-300"
+                            enter-from="opacity-0"
+                            enter-to="opacity-100"
+                            leave="ease-in duration-200"
+                            leave-from="opacity-100"
+                            leave-to="opacity-0"
+                        >
+                            <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+                        </TransitionChild>
 
-                    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-                        <div class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
-                            <TransitionChild
-                                as="template"
-                                enter="ease-out duration-300"
-                                enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                enter-to="opacity-100 translate-y-0 sm:scale-100"
-                                leave="ease-in duration-200"
-                                leave-from="opacity-100 translate-y-0 sm:scale-100"
-                                leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                            <div
+                                class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0"
                             >
-                                <DialogPanel
-                                    class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                                <TransitionChild
+                                    as="template"
+                                    enter="ease-out duration-300"
+                                    enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enter-to="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leave-from="opacity-100 translate-y-0 sm:scale-100"
+                                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                 >
-                                    <div class="sm:flex sm:items-start">
-                                        <div
-                                            class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto rounded-full sm:mx-0 sm:h-10 sm:w-10"
-                                            :class="iconColor"
-                                        >
-                                            <component :is="icon" class="w-6 h-6" aria-hidden="true" />
-                                        </div>
-                                        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                            <DialogTitle
-                                                as="h3"
-                                                class="text-base font-semibold leading-6 text-gray-900"
+                                    <DialogPanel
+                                        class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                                    >
+                                        <div class="sm:flex sm:items-start">
+                                            <div
+                                                class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto rounded-full sm:mx-0 sm:h-10 sm:w-10"
+                                                :class="iconColor"
                                             >
-                                                {{ title }}
-                                            </DialogTitle>
+                                                <component :is="icon" class="w-6 h-6" aria-hidden="true" />
+                                            </div>
+                                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                <DialogTitle
+                                                    as="h3"
+                                                    class="text-base font-semibold leading-6 text-gray-900"
+                                                >
+                                                    {{ title }}
+                                                </DialogTitle>
 
-                                            <div class="mt-2 text-sm text-gray-500">
-                                                <slot name="content">
-                                                    {{ content }}
-                                                </slot>
+                                                <div class="mt-2 text-sm text-gray-500">
+                                                    <slot name="content">
+                                                        {{ content }}
+                                                    </slot>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                                        <button
-                                            type="button"
-                                            class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold rounded-md shadow-sm sm:ml-3 sm:w-auto"
-                                            :class="buttonColor"
-                                            @click="confirm"
-                                            v-text="confirmActionLabel || trigger"
-                                        />
+                                        <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                            <button
+                                                type="button"
+                                                class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold rounded-md shadow-sm sm:ml-3 sm:w-auto"
+                                                :class="buttonColor"
+                                                @click="confirm"
+                                                v-text="confirmActionLabel || trigger"
+                                            />
 
-                                        <button
-                                            type="button"
-                                            class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                            @click="close"
-                                            ref="cancelButtonRef"
-                                            v-text="cancelActionLabel || $t('cancel')"
-                                        />
-                                    </div>
-                                </DialogPanel>
-                            </TransitionChild>
+                                            <button
+                                                type="button"
+                                                class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                                @click="close"
+                                                ref="cancelButtonRef"
+                                                v-text="cancelActionLabel || $t('cancel')"
+                                            />
+                                        </div>
+                                    </DialogPanel>
+                                </TransitionChild>
+                            </div>
                         </div>
-                    </div>
-                </Dialog>
-            </TransitionRoot>
-        </Teleport>
+                    </Dialog>
+                </TransitionRoot>
+            </Teleport>
+        </ClientOnly>
     </div>
 </template>
 
@@ -91,6 +95,7 @@
     import { useForm } from '@inertiajs/vue3';
     import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue';
     import { ExclamationIcon } from '@heroicons/vue/outline';
+    import ClientOnly from '@/Components/ClientOnly.vue';
 
     const props = defineProps({
         title: {
--- resources/js/Components/modals/ModalAction.vue
+++ resources/js/Components/modals/ModalAction.vue
@@ -4,80 +4,86 @@
             {{ triggerModalText }}
         </button>
 
-        <Teleport to="body">
-            <TransitionRoot as="template" :show="open">
-                <Dialog as="div" class="relative z-101" @close="open = false">
-                    <TransitionChild
-                        as="template"
-                        enter="ease-out duration-300"
-                        enter-from="opacity-0"
-                        enter-to="opacity-100"
-                        leave="ease-in duration-200"
-                        leave-from="opacity-100"
-                        leave-to="opacity-0"
-                    >
-                        <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
-                    </TransitionChild>
+        <ClientOnly>
+            <Teleport to="#notification-teleport-target">
+                <TransitionRoot as="template" :show="open">
+                    <Dialog as="div" class="relative z-101" @close="open = false">
+                        <TransitionChild
+                            as="template"
+                            enter="ease-out duration-300"
+                            enter-from="opacity-0"
+                            enter-to="opacity-100"
+                            leave="ease-in duration-200"
+                            leave-from="opacity-100"
+                            leave-to="opacity-0"
+                        >
+                            <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+                        </TransitionChild>
 
-                    <div class="fixed inset-0 z-10 overflow-y-auto">
-                        <div class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
-                            <TransitionChild
-                                as="template"
-                                enter="ease-out duration-300"
-                                enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                enter-to="opacity-100 translate-y-0 sm:scale-100"
-                                leave="ease-in duration-200"
-                                leave-from="opacity-100 translate-y-0 sm:scale-100"
-                                leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        <div class="fixed inset-0 z-10 overflow-y-auto">
+                            <div
+                                class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0"
                             >
-                                <DialogPanel
-                                    class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                                <TransitionChild
+                                    as="template"
+                                    enter="ease-out duration-300"
+                                    enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enter-to="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leave-from="opacity-100 translate-y-0 sm:scale-100"
+                                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                 >
-                                    <div>
-                                        <div
-                                            class="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full"
-                                        >
-                                            <CheckIcon class="w-6 h-6 text-green-500" aria-hidden="true" />
-                                        </div>
-
-                                        <div class="mt-3 text-center sm:mt-5">
-                                            <DialogTitle
-                                                as="h3"
-                                                class="text-base font-semibold leading-6 text-gray-900"
+                                    <DialogPanel
+                                        class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                                    >
+                                        <div>
+                                            <div
+                                                class="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full"
                                             >
-                                                {{ title }}
-                                            </DialogTitle>
-                                            <div class="mt-2">
-                                                <p class="text-sm text-gray-500">
-                                                    {{ body }}
-                                                </p>
+                                                <CheckIcon class="w-6 h-6 text-green-500" aria-hidden="true" />
+                                            </div>
+
+                                            <div class="mt-3 text-center sm:mt-5">
+                                                <DialogTitle
+                                                    as="h3"
+                                                    class="text-base font-semibold leading-6 text-gray-900"
+                                                >
+                                                    {{ title }}
+                                                </DialogTitle>
+                                                <div class="mt-2">
+                                                    <p class="text-sm text-gray-500">
+                                                        {{ body }}
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                                        <button
-                                            type="button"
-                                            class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                                            @click="open = false"
+                                        <div
+                                            class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3"
                                         >
-                                            {{ cancelModalText }}
-                                        </button>
+                                            <button
+                                                type="button"
+                                                class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                                                @click="open = false"
+                                            >
+                                                {{ cancelModalText }}
+                                            </button>
 
-                                        <button
-                                            @click="action"
-                                            type="button"
-                                            class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-primary-500 hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500 sm:col-start-2"
-                                        >
-                                            {{ actionModalText }}
-                                        </button>
-                                    </div>
-                                </DialogPanel>
-                            </TransitionChild>
+                                            <button
+                                                @click="action"
+                                                type="button"
+                                                class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-primary-500 hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500 sm:col-start-2"
+                                            >
+                                                {{ actionModalText }}
+                                            </button>
+                                        </div>
+                                    </DialogPanel>
+                                </TransitionChild>
+                            </div>
                         </div>
-                    </div>
-                </Dialog>
-            </TransitionRoot>
-        </Teleport>
+                    </Dialog>
+                </TransitionRoot>
+            </Teleport>
+        </ClientOnly>
     </div>
 </template>
 
@@ -91,6 +97,7 @@
     /** Import plugins. */
     import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue';
     import { CheckIcon } from '@heroicons/vue/outline';
+    import ClientOnly from '@/Components/ClientOnly.vue';
 
     /** Component props. */
     const props = defineProps({
