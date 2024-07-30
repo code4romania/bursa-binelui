6b8c59c723dc94fe076f4c68bd2a67742622d652
Andrei Ioniță <hi@andrei.io>
2024-07-30 12:53:06 +0100

    fix: map projects count (#392)

--- resources/js/Components/maps/Map.vue
+++ resources/js/Components/maps/Map.vue
@@ -1,107 +1,85 @@
 <template>
-    <div id="map" class="rounded-lg h-[550px]"></div>
+    <div id="map" class="rounded-lg h-[550px]" />
 </template>
 
 <script setup>
-/** Import from vue. */
-import { ref, onMounted } from 'vue';
-import { usePage } from '@inertiajs/vue3';
-import * as GM from '@googlemaps/markerclusterer';
-const { MarkerClusterer } = GM;
+    /** Import from vue. */
+    import { ref, onMounted } from 'vue';
+    import { usePage } from '@inertiajs/vue3';
+    import * as GM from '@googlemaps/markerclusterer';
+    const { MarkerClusterer } = GM;
 
-/** Component props. */
-const props = defineProps({ data: Array });
+    /** Component props. */
+    const props = defineProps({ data: Array });
 
-/** Component emits */
-const emit = defineEmits(['county-selected']);
+    /** Component emits */
+    const emit = defineEmits(['county-selected']);
 
-const map = ref(null);
-const selectedCounties = ref([]);
+    const map = ref(null);
+    const selectedCounties = ref([]);
 
-const page = usePage();
+    const page = usePage();
 
-/** Intialize google map. */
-async function initializeMap() {
-    return new Promise((resolve, reject) => {
-        if (window?.google !== undefined) {
-            return resolve(window.google);
-        }
-
-        const script = document.createElement('script');
-        script.src = 'https://maps.googleapis.com/maps/api/js?key=' + page.props.google_maps_api_key;
-        script.async = true;
-        script.defer = true;
-        script.onload = () => {
-            resolve(window.google);
-        };
-        script.onerror = () => {
-            reject(new Error('Failed to load Google Maps API'));
-        };
-        document.head.appendChild(script);
-    });
-}
-const filterProjects = (county) => {
-    emit('county-selected', county.id);
-};
-
-/** Generate map markers for each county */
-const generateMapMarkers = async () => {
-    /** Render map. */
-    const google = await initializeMap();
-
-    /** Map options, */
-    const mapOptions = {
-        center: { lat: 45.9432, lng: 24.9668 },
-        zoom: 7,
-        mapId: '63407f7951bfa43b',
-    };
-    const { Map } = await google.maps.importLibrary('maps');
-
-    const map = new Map(document.getElementById('map'), mapOptions);
-
-    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary('marker');
-    // const { AdvancedMarkerElement } = await google.maps.importLibrary('marker');
-    props.data.forEach((county) => {
-        {
-            console.log(county);
-            let scale = 1;
-            switch (true) {
-                case county.projects_count > 1:
-                    scale = 1.2;
-                    break;
-                case county.projects_count > 20:
-                    scale = 1.4;
-                    break;
-                case county.projects_count > 30:
-                    scale = 1.6;
-                    break;
-                case county.projects_count > 40:
-                    scale = 1.8;
-                    break;
-                case county.projects_count > 50:
-                    scale = 2;
+    /** Intialize google map. */
+    async function initializeMap() {
+        return new Promise((resolve, reject) => {
+            if (window?.google !== undefined) {
+                return resolve(window.google);
             }
-            console.log(scale);
-            const pinNoGlyph = new PinElement({
+
+            const script = document.createElement('script');
+            script.src = 'https://maps.googleapis.com/maps/api/js?key=' + page.props.google_maps_api_key;
+            script.async = true;
+            script.defer = true;
+
+            script.onload = () => {
+                resolve(window.google);
+            };
+
+            script.onerror = () => {
+                reject(new Error('Failed to load Google Maps API'));
+            };
+
+            document.head.appendChild(script);
+        });
+    }
+
+    /** Generate map markers for each county */
+    const generateMapMarkers = async () => {
+        const google = await initializeMap();
+        const { Map } = await google.maps.importLibrary('maps');
+        const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary('marker');
+
+        /** Map options, */
+        const mapOptions = {
+            center: { lat: 45.9432, lng: 24.9668 },
+            zoom: 7,
+            mapId: '63407f7951bfa43b',
+        };
+
+        const map = new Map(document.getElementById('map'), mapOptions);
+
+        props.data.forEach((county) => {
+            const pin = new PinElement({
                 glyph: `${county.projects_count}`,
                 background: '#00438A',
-                glyphColor: '#fff',
+                glyphColor: '#FFF',
                 borderColor: '#A8D2FF',
-                scale: scale,
+                scale: 1.4,
             });
-            const position = { lat: parseFloat(county.lat), lng: parseFloat(county.long) };
+
             const marker = new AdvancedMarkerElement({
-                position: position,
+                position: { lat: county.lat, lng: county.long },
                 map: map,
                 title: county.name,
-                content: pinNoGlyph.element,
+                content: pin.element,
             });
-            marker.addListener('click', () => filterProjects(county, marker));
-        }
-    });
-};
 
-onMounted(() => {
-    generateMapMarkers();
-});
+            marker.addListener('click', () => emit('county-selected', county.id));
+        });
+    };
+
+    onMounted(() => {
+        generateMapMarkers();
+    });
 </script>
